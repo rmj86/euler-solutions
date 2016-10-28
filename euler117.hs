33420b895cd164c099554768c3251922fe2dfa25
rmj86 <johannes.rehnstrom.1986@gmail.com>
2016-10-28 20:55:21 +0200

    onelinerized

--- euler117.hs
+++ euler117.hs
@@ -10,18 +10,9 @@ How many ways can a row measuring fifty units in length be tiled?
 
 NOTE: This is related to Problem 116.
 -------------------------------------------------------------------------------}
+import Data.List (tails, transpose)
 
-import qualified Data.Map.Lazy as Map
-import Data.Map.Lazy ((!))
+tilings = 1:1:2:4:(map sum . transpose . take 4 . tails $ tilings)
 
-memo = Map.fromList [(n,tilings n) | n<-[0..50]]
-
-tilings 0 = 1
-tilings 1 = 1
-tilings 2 = 2
-tilings 3 = 4
-tilings n = memo!(n-1) + memo!(n-2) + memo!(n-3) + memo!(n-4)
-
-solution = tilings 50
-
-main = print solution
+-------------------
+main = print (tilings !! 50)
