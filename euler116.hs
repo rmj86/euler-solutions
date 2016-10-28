e37dbd81d4c3fb2e78a4d7879b65a82f5b3cc478
rmj86 <johannes.rehnstrom.1986@gmail.com>
2016-10-28 21:58:12 +0200

    prettified

--- euler116.hs
+++ euler116.hs
@@ -36,5 +36,17 @@ green = tiling 3
 blue = tiling 4
 
 solution = red 50 + green 50 + blue 50 - 3
+--------------------------
 
-main = print solution
+tilings' w = ts
+  where ts = (replicate w 1) ++ zipWith (+) ts (drop (w-1) ts)
+
+red' = tilings' 2
+green' = tilings' 3
+blue' = tilings' 4
+
+sol = red'!!50 + green'!!50 + blue'!!50 - 3
+
+main = do
+    print solution
+    print sol
